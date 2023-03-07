78568d9172a4c0d9163250fd01c98f7e9c2fa2c4
Agon Veselaj <masterthe844@gmail.com>
2023-03-07 10:51:05 +0000

    Push Numer2 hja Agon

--- src/LibrarySystem/FacultyMember.java
+++ src/LibrarySystem/FacultyMember.java
@@ -1,6 +1,14 @@
 package LibrarySystem;
 
 public class FacultyMember {
-    
+    public String name;
+    public String department;
 
+    public String getDepartment() {
+        public void setDepartment ('String department');
+    }
+
+    {
+        this.department = department;
+    }
 }
--- src/LibrarySystem/Lending.java
+++ src/LibrarySystem/Lending.java
@@ -2,6 +2,17 @@ package LibrarySystem;
 
 public class Lending {
     public static void main(String[] args) {
+        public Lending(Book book, User user); {
+            public localDateTime getDueDate (); {
+                public void setDueDate (LocalDate dueDate);
+                public Book getBook (); {
+                    public setBook(Book book);
+                    public User getUser (); {
+                        public setUser(User user);
+                    }
+                }
 
+            }
+        }
     }
 }
