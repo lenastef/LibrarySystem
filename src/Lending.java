2cc43a4d11b545c7d02296661792ebc887726041
helena stefansdottir <hel>
2023-03-07 10:28:51 +0000

    Búa til classes

---
+++ src/Lending.java
@@ -0,0 +1,5 @@
+public class Lending {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Student.java
@@ -0,0 +1,18 @@
+package LibrarySystem;
+
+public class Student extends User {
+    private boolean feePaid;
+
+    public boolean isFeePaid() {
+        return feePaid;
+    }
+
+    public void setFeePaid(boolean feePaid) {
+        this.feePaid = feePaid;
+    }
+
+    public Student(String name, boolean feePaid) {
+        super(name);
+        this.feePaid = feePaid;
+    }
+}
